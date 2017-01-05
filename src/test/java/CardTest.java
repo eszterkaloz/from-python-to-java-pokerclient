b7854cf07e8174d99e59dbbeec2ee9317edff22b
eszterkaloz <eszterkaloz@gmail.com>
2017-01-05 15:48:37 +0100

    Implemented tests for Card

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -2,13 +2,38 @@ import org.junit.*;
 
 
 public class CardTest {
-    @Test
-    public void testInitiatingCard() throws Exception {
+    private Card queenOfHearts;
+    private Card sixOfSpades;
 
+    @Before
+    public void setUp() throws Exception {
+        queenOfHearts = new Card("hq");
+        sixOfSpades = new Card("s6");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInitiatingCardWithWrongCardColor() throws Exception {
+        sixOfSpades = new Card("e6");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInitiatingCardWithWrongCardValue() throws Exception {
+        queenOfHearts = new Card("h1");
     }
 
     @Test
-    public void testGetValue() throws Exception {
+    public void testGetValueOfHQ() throws Exception {
+        Assert.assertEquals(12, queenOfHearts.getValue());
+    }
 
+    @Test
+    public void testGetValueOfS6() throws Exception {
+        Assert.assertEquals(6, sixOfSpades.getValue());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        queenOfHearts = null;
+        sixOfSpades = null;
     }
 }
