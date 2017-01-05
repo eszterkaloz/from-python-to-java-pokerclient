1cd8f9f57a23e1014bd049358a0170d27cd78f09
eszterkaloz <eszterkaloz@gmail.com>
2017-01-05 16:06:04 +0100

    Implemented tests for PokerClient

--- src/test/java/PokerClientTest.java
+++ src/test/java/PokerClientTest.java
@@ -2,8 +2,25 @@ import org.junit.*;
 
 
 public class PokerClientTest {
-    @Test
-    public void testHighestCardIsMine() throws Exception {
+    private PokerClient pokerClient;
 
+    @Before
+    public void setUp() throws Exception {
+        pokerClient = new PokerClient("h8", "h9", "h10", "hj", "hq");
+    }
+
+    @Test
+    public void testHighestCardIsMineTrue() throws Exception {
+        Assert.assertEquals(true, pokerClient.highestCardIsMine("h2", "c3", "s2", "cj", "d8"));
+    }
+
+    @Test
+    public void testHighestCardIsMineFalse() throws Exception {
+        Assert.assertEquals(false, pokerClient.highestCardIsMine("d10", "dj", "dq", "dk", "da"));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        pokerClient = null;
     }
 }
