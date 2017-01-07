7321dfe3c953fca0193ae4940db28e7a56c9edec
eszterkaloz <eszterkaloz@gmail.com>
2017-01-07 15:38:15 +0100

    Refactored PokerClient

--- src/main/java/PokerClient.java
+++ src/main/java/PokerClient.java
@@ -8,37 +8,37 @@ public class PokerClient {
     private List<Card> pokerClientCards = new ArrayList<>();
 
     public PokerClient(String p1, String p2, String p3, String p4, String p5) {
+
         for (String card : Arrays.asList(p1, p2, p3, p4, p5)) {
             pokerClientCards.add(new Card(card.toUpperCase()));
         }
+
     }
 
     public boolean highestCardIsMine(String p1, String p2, String p3, String p4, String p5) {
-        Card hc = new Card("s2");
-        List<Card> o = new ArrayList<Card>();
-        o.add(new Card(p1.toUpperCase()));
-        o.add(new Card(p2.toUpperCase()));
-        o.add(new Card(p3.toUpperCase()));
-        o.add(new Card(p4.toUpperCase()));
-        o.add(new Card(p5.toUpperCase()));
+        List<Card> otherCards = new ArrayList<>();
 
-        for (int i = 0; i < o.size(); i++) {
-            Card mc = pokerClientCards.get(i);
-            for (int j = 0; j < o.size(); j++) {
-                Card oc = o.get(j);
-                if (oc.getValue() > mc.getValue()) {
-                    if (oc.getValue() > hc.getValue()) {
-                        hc = oc;
+        for (String card : Arrays.asList(p1, p2, p3, p4, p5)) {
+            otherCards.add(new Card(card.toUpperCase()));
+        }
+
+        Card highestCard = otherCards.get(0);
+
+        for (Card myCard : pokerClientCards) {
+            for (Card otherCard : otherCards) {
+                if (otherCard.getValue() > myCard.getValue()) {
+                    if (otherCard.getValue() > highestCard.getValue()) {
+                        highestCard = otherCard;
                     }
                 } else {
-                    if (mc.getValue() > hc.getValue()) {
-                        hc = mc;
+                    if (myCard.getValue() > highestCard.getValue()) {
+                        highestCard = myCard;
                     }
                 }
             }
         }
 
-        return pokerClientCards.contains(hc);
+        return pokerClientCards.contains(highestCard);
     }
 
 }
